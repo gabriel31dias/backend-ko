87138c9bde3ea90c1b12dc5e165e2b08e17424e4
Gabriel Dias <diassilvag31@gmail.com>
2025-12-10 01:03:32 -0300

    feat: fix back

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+if (typeof process.loadEnvFile === 'function') {
+  process.loadEnvFile();
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
